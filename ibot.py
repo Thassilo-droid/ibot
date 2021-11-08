673157613eb991a7dff167d70c7e2057bafe5c3e
thassi <58367448+Thassilo-droid@users.noreply.github.com>
2021-11-08 22:01:50 +0100

    Update ibot.py

--- ibot.py
+++ ibot.py
@@ -4,5 +4,5 @@ from source.Destroyer import Destroyer
 Destroyer.destroyConfig()
 
 bot = Bot()
-bot.login(username="riseandbusiness", password="PaulWalcher69")
+bot.login(username="yourname", password="yourpassword")
 bot.upload_photo("frog_sitting_0.jpg", caption="Nice pic!")
