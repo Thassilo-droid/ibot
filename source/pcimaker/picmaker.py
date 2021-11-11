0a9138edce544897454b31b0785d0c8b08e4caca
thassi <thassilo.horn@gmx.de>
2021-11-11 22:02:44 +0100

    autosave

--- source/pcimaker/picmaker.py
+++ source/pcimaker/picmaker.py
@@ -1,10 +1,10 @@
 from PIL import Image, ImageFilter, ImageFont, ImageDraw
 import time
 
-'''size = 1080, 1080'''
+size = 1080, 1080
 
 '''n dient als counter für bilder'''
-n = 1
+n = 3
 
 #try:
 
@@ -12,11 +12,16 @@ n = 1
 
 image = Image.open("//home//thassi//projects//ibot//source//pcimaker//pictures_original//" + str(n) + ".jpg")
 
+image.thumbnail(size,Image.ANTIALIAS)
+
+image = image.convert(mode='L')
 image = image.filter(ImageFilter.GaussianBlur(20))
 
-font_type = ImageFont.truetype('StayWriter-Handmade.ttf',200)
+image = image.convert('RGB')
+
+font_type = ImageFont.truetype('StayWriter-Handmade.ttf',50)
 draw = ImageDraw.Draw(image)
-draw.text(xy=(300,300), text = "motivierender Spruch", fill=(255,215,0), font=font_type)
+draw.text(xy=tuple(el/3 for el in size), text = "motivierender Spruch", fill=(255,215,0), font=font_type)
 
 image.save("//home//thassi//projects//ibot//source//pcimaker//pictures_new//" + str(n) + ".jpg")
 #n += 1
